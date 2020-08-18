680e3c85387473f23e2050a0d1fe565b3c69e2ae
chasimyeong <casim0@naver.com>
2020-08-18 16:41:57 +0900

    main.py로 바뀔 예정

--- Dpredict_RestAPI_v0.6.py
+++ Dpredict_RestAPI_v0.6.py
@@ -14,8 +14,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 from dtnn import dtnn
-from dtnn import utils
-from dtnn import spatial_analysis as sa
+from dtsa import spatial_analysis as sa
 
 warnings.filterwarnings('ignore')
 
@@ -32,37 +31,14 @@ def predict():
 
         # parameter
         data = request.form
-        command = data['command']
-        img_format = data['format']
 
-        result = {}
-
-        if command == 'skyline_detection':
-
-            skyline = dtnn.SkylineDetection(img, sky_model)
-            output_img = skyline.predict(threshold=20)
-
-        elif command == 'view_shielding_rate':
-
-            shielding = dtnn.ViewShieldingRate(img, shield_model)
-            output_img, shielding_rate = shielding.predict(threshold=20)
-            result['shielding_rate'] = shielding_rate
-
-        else:
-            command = "The 'command' parameters that we support are 'skyline_detection', " \
-                      "'view_shielding_rate'"
-
-            return jsonify({'Error': command})
-
-        output_format = utils.OutputFormat(output_img, img_format)
-        final_img = output_format.trans_format()
-        result['output_img'] = final_img
-
-        return jsonify({'command': command, 'result': result})
+        response = dtnn.check_command(img, data, models)
 
     else:
         error = 'Error : Request method is only POST'
-        return jsonify({'Error': error})
+        response = jsonify({'Error': error})
+
+    return response
 
 
 @app.route('/spatial-analysis', methods=['GET', 'POST'])
@@ -70,24 +46,16 @@ def spatial_analysis():
     if request.method == 'POST':
 
         data = request.form
-        command = data['command']
-        param = data['parameter']
 
-        if command == "linear_line_of_sight":
-            LLOS = sa.LinearLineOfSight(param['fileName'], param['coord'], param['boundingBox'],
-                                        param['observerPoint'], param['observerOffset'], param['targetPoint'])
-            response = LLOS.analysis()
-        elif command == "calculate_cut_fill":
-            CCF = sa.CalculateCutFill(param['fileName'], param['coord'], param['boundingBox'],
-                                      param['inputGeometry'], param['userMeanHeight'])
-            response = CCF.analysis()
+        response = sa.check_command(data)
 
-        else:
-            response = "The 'command' parameters that we support are 'linear_line_of_sight', " \
-                       "'calculate_cut_fill'"
-        return jsonify({'command': command, 'result': response})
+    else:
+        error = 'Error : Request method is only POST'
+        response = jsonify({'Error': error})
+
+    return response
 
 
 if __name__ == "__main__":
-    sky_model, shield_model = dtnn.load_models()
+    models = dtnn.load_models()
     app.run(host='0.0.0.0')
